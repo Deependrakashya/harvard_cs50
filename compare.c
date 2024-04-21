c718488497849b6fff5576930121c7303c53f36f
Deependra Kashyap <113233056+Deependrakashya@users.noreply.github.com>
2024-04-21 19:50:16 +0000

    compare

---
+++ compare.c
@@ -0,0 +1,16 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main (void){
+    int x=get_int("what is x? ");
+    int y =get_int("what is y? ");
+    if(x>y){
+     printf("x is larger than y \n");
+    }
+     else  if(x<y){
+    printf("y is larger than x\n");
+    }
+     else  {
+ printf("x is equal to y\n");
+    }
+}
